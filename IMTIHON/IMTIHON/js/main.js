38e49384ec615271e4df2fd84ff347d95c06083c
1slombek2009 <130884952+1slombek2009@users.noreply.github.com>
2023-07-24 13:52:55 +0500

    Add files via upload

---
+++ IMTIHON/IMTIHON/js/main.js
@@ -0,0 +1,66 @@
+$(".owl-dish").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1200: {
+      items: 4,
+    },
+  },
+});
+
+$(".owl-cust").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1200: {
+      items: 3,
+    },
+  },
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  autoplay: true,
+  autoplayTimeout: 1500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1200: {
+      items: 4.7,
+    },
+  },
+});
+
+window.addEventListener("load", () => {
+  document.querySelector(".loader").style.display = "none"
+})
